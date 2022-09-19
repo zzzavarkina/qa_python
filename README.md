190406f0873917a97cc7f5fdb765c24360c86b04
zzzavarkina <writing-beast@yandex.ru>
2022-09-19 14:08:53 +0300

    first_commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# qa_python
+# Проверки для класса BooksCollector
+Сделано 16 проверок.
+### test_add_new_book_add_two_books
+Проверка добавления книги. Даже двух книг.
+### test_books_rating_and_favorites_true
+Проверка метода __init__.
+###  test_add_new_book_not_empty_name_rating_is_one
+Устанавливается ли по умолчанию при добавлении книги с непустым именем рейтинг 1.
+###  test_set_book_rating_for_added_book_rating_5_rating_is_set
+Можно ли установить рейтинг 5 для книги, добавленной в словарь.
+###  test_get_books_with_specific_rating_rating_5_return_list_of_books
+Возвращается ли список книг с рейтингом 5.
+###  test_get_books_rating_true
+Возвращается ли словарь целиком.
+###  test_add_book_in_favorites_added_book_name_book_in_favorites
+Добавляется ли книга, добавленная в словарь, в Избранное.
+###  test_delete_book_from_favorites_added_book_name_book_deleted
+Удаляется ли книга, добавленная в словарь и Избранное, из Избранного.
+###  test_get_list_of_favorites_books_true
+Возвращается ли список книг, добавленных в Избранное.
+###  test_get_book_rating_for_empty_name_return_rating
+Возвращается ли рейтинг книги для пустого имени.
+###  test_add_new_book_add_book_twice_not_in_books_rating
+Можно ли дважды добавить в словарь одну и ту же книгу.
+###  test_set_book_rating_for_not_added_book_rating_not_set
+Устанавливается ли рейтинг для книги, не добавленной в словарь.
+###  test_set_book_rating_rating_less_than_one_rating_not_set
+Устанавливается ли рейтинг 0.
+###  test_set_book_rating_rating_more_than_ten_rating_not_set
+Устанавливается ли рейтинг 11.
+###  test_get_book_rating_for_not_added_book_return_none
+Возвращается ли None, если передать имя не добавленной в словарь книги.
+###  test_add_book_in_favorites_not_added_book_return_none
+Добавляется ли книга в Избранное, если ее нет в словаре.
--- tests.py
+++ tests.py
@@ -1,24 +1,84 @@
-from main import BooksCollector
-
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
-        # добавляем две книги
+    def test_add_new_book_add_two_books(self, collector):
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_books_rating_and_favorites_true(self, collector):
+        assert collector.books_rating == {} and collector.favorites == []
+
+    def test_add_new_book_not_empty_name_rating_is_one(self, collector):
+        collector.add_new_book('Мегеры')
+        assert collector.get_book_rating('Мегеры') == 1
+
+    def test_set_book_rating_for_added_book_rating_5_rating_is_set(self, collector):
+        collector.add_new_book('Шесть минут до конца лета')
+        collector.set_book_rating('Шесть минут до конца лета', 5)
+        assert collector.books_rating['Шесть минут до конца лета'] == 5
+
+    def test_get_books_with_specific_rating_rating_5_return_list_of_books(self, collector):
+        collector.add_new_book('Колледж святого Джозефа')
+        collector.add_new_book('Чернила')
+        collector.add_new_book('Хюльдра')
+        collector.set_book_rating('Колледж святого Джозефа', 5)
+        collector.set_book_rating('Чернила', 5)
+        collector.set_book_rating('Хюльдра', 5)
+        assert len(collector.get_books_with_specific_rating(5)) == 3
+
+    def test_get_books_rating_true(self, collector):
+        collector.add_new_book('Диалог')
+        collector.add_new_book('Слово живое и мертвое')
+        collector.add_new_book('Как писать хорошо')
+        assert collector.get_books_rating() != {} and len(collector.get_books_rating()) == 3
+
+    def test_add_book_in_favorites_added_book_name_book_in_favorites(self, collector):
+        collector.add_new_book('Психбольница в руках пациентов')
+        collector.add_book_in_favorites('Психбольница в руках пациентов')
+        assert 'Психбольница в руках пациентов' in collector.get_list_of_favorites_books()
+
+    def test_delete_book_from_favorites_added_book_name_book_deleted(self, collector):
+        collector.add_new_book('Все о муми-троллях')
+        collector.add_book_in_favorites('Все о муми-троллях')
+        collector.delete_book_from_favorites('Все о муми-троллях')
+        assert 'Все о муми-троллях' not in collector.get_list_of_favorites_books()
+
+    def test_get_list_of_favorites_books_true(self, collector):
+        collector.add_new_book('Острые предметы')
+        collector.add_new_book('Исчезнувшая')
+        collector.add_new_book('Темные тайны')
+        collector.add_book_in_favorites('Острые предметы')
+        collector.add_book_in_favorites('Исчезнувшая')
+        collector.add_book_in_favorites('Темные тайны')
+        assert len(collector.get_list_of_favorites_books()) == 3
+
+    def test_get_book_rating_for_empty_name_return_rating(self, collector):
+        collector.add_new_book('')
+        collector.set_book_rating('', 5)
+        assert collector.get_book_rating('') == 5
+
+    def test_add_new_book_add_book_twice_not_in_books_rating(self, collector):
+        collector.add_new_book('Русский балет Дягилева')
+        collector.add_new_book('Русский балет Дягилева')
+        assert len(collector.get_books_rating()) == 1
+
+    def test_set_book_rating_for_not_added_book_rating_not_set(self, collector):
+        collector.set_book_rating('Несказочная проза провинциального города', 8)
+        assert collector.get_book_rating('Несказочная проза провинциального города') == None
+
+    def test_set_book_rating_rating_less_than_one_rating_not_set(self, collector):
+        collector.add_new_book('Заводной апельсин')
+        collector.set_book_rating('Заводной апельсин', 0)
+        assert collector.get_book_rating('Заводной апельсин') == 1
+    def test_set_book_rating_rating_more_than_ten_rating_not_set(self, collector):
+        collector.add_new_book('Бремя страстей человеческих')
+        collector.set_book_rating('Бремя страстей человеческих', 11)
+        assert collector.get_book_rating('Бремя страстей человеческих') == 1
+
+    def test_get_book_rating_for_not_added_book_return_none(self, collector):
+        assert collector.get_book_rating('Несказочная проза провинциального города') == None
+    def test_add_book_in_favorites_not_added_book_return_none(self, collector):
+        collector.add_book_in_favorites('Крестный отец')
+        assert 'Крестный отец' not in collector.get_list_of_favorites_books()
+
+
